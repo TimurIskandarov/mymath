754e20fbc5708b9684a3ceaaec3dcbe5716d8bde
U-LASPACE\IskandarovTR <IskandarovTR@ARM-146647.laspace.ru>
2023-09-20 17:20:03 +0300

    fix: add abs, max, yn functions

--- math.go
+++ math.go
@@ -6,3 +6,15 @@ import "math"
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
